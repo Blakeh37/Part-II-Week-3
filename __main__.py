b971f48c584740fffff9b2066e8867afaf5cfa0b
stephsasue709 <noe21002@byui.edu>
2021-10-02 12:03:45 -0400

    Merge branch 'main' of https://github.com/Blakeh37/Part-II-Week-3

--- __main__.py
+++ __main__.py
@@ -3,10 +3,12 @@ def main():
     from hollingsworth import blakes_greeting  
     from sander import greeting
     from hilamatu import hyrum_greeting
+    from noel import greet
     print("Welcome to our collaborative program.")
 
     blakes_greeting()
     greeting()
     hyrum_greeting()
+    greet()
 
 main()
