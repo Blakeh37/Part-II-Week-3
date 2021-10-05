c5109d527d51386b64e40d389a53077f9084bd29
Blake Hollingsworth <91762222+Blakeh37@users.noreply.github.com>
2021-10-05 11:38:01 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,11 +4,12 @@ def main():
     from sander import greeting
     from hilamatu import hyrum_greeting
     from noel import greet
+    from charlet import greeting as gr
     print("Welcome to our collaborative program.")
 
     blakes_greeting()
     greeting()
     hyrum_greeting()
     greet()
-
+    gr()
 main()
